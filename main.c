5e134b7c8ff12d20e353122bd51074b193ca54f2
Shanjin Yang <shanjin.yang@aispeech.com>
2016-10-12 15:42:02 +0800

    add current music point set
    Signed-off-by: Shanjin Yang <shanjin.yang@aispeech.com>

--- main.c
+++ main.c
@@ -7,10 +7,59 @@
 #include "json_op.h"
 #include "music_list.h"
 
-int music_list_current_position(struct op *o, music_obj *m)
+#define debug 0
+
+int music_list_current_position_get(struct op *o)
 {
+	music_obj *m = (music_obj *)op_context_get(o);
 	int retvalue = 1;
-	return 0;
+	music_info *tmp;
+	if ((o == NULL) || (m == NULL)) {
+		retvalue = -1;
+		print("error\n");
+		goto end;
+	}
+	/*get music list current node*/
+	retvalue = op_high_output(o, 10000);
+	if (retvalue == -1) {
+		print("error\n");
+		goto end;
+	}
+
+	/*for music list current point move to beginning*/
+	tmp = music_cur_get(m);
+	if (tmp != NULL) {
+		while (1) {
+			print("[title:artist:url] [%s : %s : %s]\n",
+			tmp->title, tmp->artist, tmp->url);
+			if (!strncmp(m->cur_tmp->title, tmp->title,
+					strlen(tmp->title)) &&
+			    !strncmp(m->cur_tmp->artist, tmp->artist,
+			    		strlen(tmp->artist)) &&
+			    (!strncmp(m->cur_tmp->url, tmp->url,
+			    		strlen(tmp->url)))) {
+				/*XXX set current music list position*/
+				print("found the current node\n");
+				music_cur_set(m, tmp);
+				free(m->cur_tmp->title);
+				free(m->cur_tmp->artist);
+				free(m->cur_tmp->url);
+				break;
+				} else {
+			}
+
+			tmp = music_prev_get(m);
+			if (tmp == NULL) {
+				print("no found the current node\n");
+				break;
+			}
+		}
+	} else {
+		print("no node\n");
+		goto end;
+	}
+end:
+	return retvalue;
 }
 
 int music_list_init(music_obj *m)
@@ -74,8 +123,9 @@ int high_output_cb(void *context, char *a, char *b, char *c)
 		retvalue = -1;
 		goto end;
 	}
-
+#if debug
 	print("[%s] [%s] [%s]\n", a, b, c);
+#endif
 	music_info *tmp;
 	music_info_alloc(&tmp, a, b, c);
 	music_list_insert(m, tmp);
@@ -96,6 +146,7 @@ int cur_output_cb(void *context, char *a, char *b, char *c)
 	}
 
 	print("[%s] [%s] [%s]\n", a, b, c);
+	/*XXX: recycle memeory*/
 	m->cur_tmp->title = strdup(a);
 	m->cur_tmp->artist = strdup(b);
 	m->cur_tmp->url = strdup(c);
@@ -124,7 +175,7 @@ end:
 	return retvalue;
 }
 
-int node_get(struct op *o, music_obj *m)
+int high_node_get(struct op *o, music_obj *m)
 {
 	int retvalue = 1;
 	int i = 0;
@@ -137,6 +188,7 @@ int node_get(struct op *o, music_obj *m)
 	/*for music list current point move to beginning*/
 	tmp = music_cur_get(m);
 	if (tmp != NULL) {
+		print("insert current node to file:\n");
 		print("[title:artist:url] [%s : %s : %s]\n",
 		tmp->title, tmp->artist, tmp->url);
 
@@ -153,7 +205,7 @@ int node_get(struct op *o, music_obj *m)
 			break;
 	}
 
-	print("insert node:\n");
+	print("insert node to file:\n");
 	/*insert first music list node*/
 	tmp = music_cur_get(m);
 	if (tmp != NULL) {
@@ -192,11 +244,75 @@ int machine_close(struct op *o, music_obj *m)
 		return 0;
 	}
 	int retvalue = 1;
-	retvalue = node_get(o, m);
+	retvalue = high_node_get(o, m);
 	if (retvalue == -1) {
 		goto end;
 	}
 	op_low_output(o);
+end:
+	return retvalue;
+}
+
+int music_list_cur_prev_print(music_obj *m)
+{
+	int retvalue = 1;
+	if (m == NULL) {
+		retvalue = -1;
+		print("error\n");
+		goto end;
+	}
+
+	music_info *tmp = music_cur_get(m);
+	if (tmp != NULL) {
+		print("[title:artist:url] [%s : %s : %s]\n",
+			tmp->title, tmp->artist, tmp->url);
+	} else {
+		retvalue = -1;
+		print("error\n");
+		goto end;
+	}
+	while (1) {
+		tmp = music_prev_get(m);
+		if (tmp == NULL) {
+			print("node end\n");
+			break;
+		} else {
+			print("[title:artist:url] [%s : %s : %s]\n",
+				tmp->title, tmp->artist, tmp->url);
+		}
+	}
+end:
+	return retvalue;
+}
+
+int music_list_cur_next_print(music_obj *m)
+{
+	int retvalue = 1;
+	if (m == NULL) {
+		retvalue = -1;
+		print("error\n");
+		goto end;
+	}
+
+	music_info *tmp = music_cur_get(m);
+	if (tmp != NULL) {
+		print("[title:artist:url] [%s : %s : %s]\n",
+			tmp->title, tmp->artist, tmp->url);
+	} else {
+		retvalue = -1;
+		print("error\n");
+		goto end;
+	}
+	while (1) {
+		tmp = music_next_get(m);
+		if (tmp == NULL) {
+			print("node end\n");
+			break;
+		} else {
+			print("[title:artist:url] [%s : %s : %s]\n",
+				tmp->title, tmp->artist, tmp->url);
+		}
+	}
 end:
 	return retvalue;
 }
@@ -220,6 +336,9 @@ int machine_open(struct op *o)
 	for (i = 0; i < 20; i++) {
 		op_high_output(o, i);
 	}
+	music_list_current_position_get(o);
+	music_obj *m = op_context_get(o);
+	music_list_cur_next_print(m);
 end:
 	return retvalue;
 }
@@ -282,6 +401,7 @@ int main()
 	op_reg_low_output(o_obj, low_output_cb);
 	op_reg_high_output(o_obj, high_output_cb);
 	op_reg_low_input(o_obj, low_input_cb);
+	op_reg_cur_output(o_obj, cur_output_cb);
 
 	/*machine open test*/
 	machine_open(o_obj);
